a8a5efb7ba42d7ef43755e5652ff9a2b21350591
nnvncg <congnvnctk37@gmail.com>
2017-08-22 21:13:57 +0700

    giao diện khách hàng

---
+++ WebsiteNhaHang/WebsiteNhaHang/Controllers/TuyenDungController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebsiteNhaHang.Controllers
+{
+    public class TuyenDungController : Controller
+    {
+        // GET: TuyenDung
+        public ActionResult TuyenDung()
+        {
+            return View();
+        }
+    }
+}
